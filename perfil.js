05db13287e2e14e2c59b7cce4abdc9bbddb9871f
davidineli857-ops <davidineli857@gmail.com>
2026-08-08 15:06:59 -0300

    update

--- perfil.js
+++ perfil.js
@@ -1,5 +1,18 @@
 function noturno() {
    document.body.classList.toggle("escuro")
  
-   
+ 
+  
 }
+
+ let padrao = document.querySelector("#tema")
+  
+ padrao.addEventListener ("click",function troca() {
+      if (padrao.value == "Modo Escuro") {
+         padrao.value = "Padrão"
+      }     else if (padrao.value = "Padrão") {
+       padrao.value = "Modo Escuro"
+      }
+ }
+)
+
